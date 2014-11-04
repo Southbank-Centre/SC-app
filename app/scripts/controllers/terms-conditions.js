45740e8ac31c866e2b0652319881fe9a2facafaf
Dave Vernon <dave.vernon@southbankcentre.co.uk>
2014-11-04 16:55:20 +0000

    base angular app

---
+++ app/scripts/controllers/terms-conditions.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wowApp.controller:TermsConditionsCtrl
+ * @description
+ * # TermsConditionsCtrl
+ * Controller of the wowApp
+ */
+angular.module('wowApp')
+  .controller('TermsConditionsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
