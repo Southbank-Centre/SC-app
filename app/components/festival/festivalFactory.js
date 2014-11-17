66dbce8a7aecc5da8ddef8dbc8562bccc291753c
Sam Murray <samueljmurray@gmail.com>
2014-11-17 17:36:12 +0000

    added festival factory

---
+++ app/components/festival/festivalFactory.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wowApp.factory:festivalFactory
+ * @description
+ * # festivalFactory
+ * Factory for loading festival data into the wowApp
+ */
+
+angular.module('wowApp')
+  .factory('festivalFactory', function($http) {
+
+    return {
+
+      getFestivalSingle: function(festivalId, callbackSuccess, callbackError) {
+
+        // Get request URL will be something like: 'http://wow.southbankcentre.co.uk/api/festival/'+festivalId
+        $http.get('json-test/festival-json-'+festivalId+'.json')
+          .success(callbackSuccess)
+          .error(callbackError);
+
+      }
+
+    };
+    
+  });
