11aaec2312f2892470c0e21d139dee703585608d
Sam Murray <samueljmurray@gmail.com>
2014-11-19 15:36:00 +0000

    added homepage controller which gets festival data

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -8,12 +8,37 @@
  * Controller of the wowApp
  */
 angular.module('wowApp')
-  .controller('HomeCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('HomeCtrl', function ($rootScope, $scope, festivalFactory) {
+    
+    /**
+     * Method for getting one festival from the API
+     */
+    
+    // ID of WOW Festival stored in the backend
+    var festivalId = 1;
+
+    festivalFactory.getFestivalSingle(festivalId, function(data) {
+
+      // Validation
+      // Location, event name and start date must be present for the event to display
+      if (!data.startDate || !data.festivalName) {
+        $rootScope.$broadcast('event:pageNotFound');
+      }
+
+      // Success
+      // Attach the event data to the scope
+      $scope.festival = data;
+
+    }, function(data, status) {
+
+      // Failure
+      // If event not found
+      if (status === 404) {
+        // Broadcast the pageNotFound event
+        $rootScope.$broadcast('event:pageNotFound');
+      }
+
+    });
 
     // SEO REQUIREMENT: 
     // PhantomJS pre-rendering workflow requires the page to declare, through htmlReady(), that
