f3ca3d46ceafd7f2fd145f07a7e6b5a00bd840ec
Dave Vernon <dave.vernon@southbankcentre.co.uk>
2014-11-25 10:12:32 +0000

    added offers josn to eventFactory

--- app/components/event/eventFactory.js
+++ app/components/event/eventFactory.js
@@ -13,11 +13,59 @@ angular.module('wowApp')
 
     return {
 
-      getEventSingle: function(eventId, callbackSuccess, callbackError) {
+      /* getEventSingle: function(eventId, callbackSuccess, callbackError) {
 
         // Get request URL will be something like: 'http://wow.southbankcentre.co.uk/api/event/'+eventId
         $http.get('json-test/event-test-'+eventId+'.json')
           .success(callbackSuccess)
+          .error(callbackError);
+
+      }, */
+
+      getEventSingle: function(eventId, callbackSuccess, callbackError) {
+
+        // Get request URL will be something like: 'http://wow.southbankcentre.co.uk/api/event/'+eventId
+        $http.get('json-test/'+eventId+'.json')
+          .success(function(performance) {
+
+            // Make a second call to get the performance's related production
+            // using the URL of the production's JSON endpoint
+            $http.get('json-test/'+performance.field_production.id+'.json')
+              .success(function(production) {
+
+                // inject production data into the event scope
+                performance.field_production = production;
+
+                // Make a third call to get the performance's available tickets
+                // using the URL of the ticket offer's paragraph JSON endpoint
+                // NB this should be in a for loop to load all available JSONs
+                $http.get('json-test/'+performance.field_offers[0].id+'.json')
+                  .success(function(tickets) {
+
+                    // inject ticket data into the event scope
+                    performance.field_offers[0] = tickets;
+
+                  });
+
+                // Make a fourth (?!) call to get the performance's available tickets
+                // using the URL of the ticket offer's paragraph JSON endpoint
+                $http.get('json-test/'+performance.field_offers[1].id+'.json')
+                  .success(function(tickets) {
+
+                    // inject ticket data into the event scope
+                    performance.field_offers[1] = tickets;
+                    
+                    var event = performance;
+                    // Call the callback which is passed in from EventSingleCtrl
+                    callbackSuccess(event);
+
+                  });
+
+
+              });
+
+          })
+
           .error(callbackError);
 
       },
