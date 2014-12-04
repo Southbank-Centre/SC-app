b0925fa1158c760ccbf19bcba5f75d7988cd98f1
Dave Vernon <dave.vernon@southbankcentre.co.uk>
2014-12-04 14:02:06 +0000

    removed uneeded variables and convert timestamp string back to number

--- app/components/festival/festivalFactory.js
+++ app/components/festival/festivalFactory.js
@@ -18,17 +18,14 @@ angular.module('wowApp')
         $http.get('/json/api/festival/'+$rootScope.festivalId)
           .success(function(festival) {
 
-            var s1 = festival.field_date_start*1000; // need start date as number, not string
-            var e1 = festival.field_date_end*1000; //// need end date as number, not string
-
             // Correct date format for start and end dates
             festival.field_date_start = utilities.timestampSecondsToMS(festival.field_date_start);
             festival.field_date_end = utilities.timestampSecondsToMS(festival.field_date_end);
 
             // Convert festival duration into array of days for use by events list filter
             // function getAllDays() {
-              var s = new Date(s1);
-              var e = new Date(e1);
+              var s = new Date(Number(festival.field_date_start));
+              var e = new Date(Number(festival.field_date_end));
               var a = [];
 
               while (s <= e) {
