75a5c3352db100cf5df894ac9d92b7d6ae202b60
samueljmurray <samueljmurray@gmail.com>
2015-01-09 14:56:03 +0000

    added filter which formats numbers to 2 decimal places if they required decimal points

--- app/app.filter.js
+++ app/app.filter.js
@@ -14,4 +14,30 @@ angular
 
     return $sce.trustAsHtml;
 
+  })
+  /**
+   * @ngdoc filter
+   * @name wowApp.filter:twoDecimalPlacesIfFixed
+   * @filter
+   *
+   * @description
+   * Add 2 decimal points if the number requires them.
+   * E.g. 20.00 = 20; 40.5 = 40.50
+   */
+  .filter('twoDecimalPlacesIfFixed', function() {
+
+    return function (number) {
+
+      var num = parseFloat(number);
+      var num2 = num.toFixed(2);
+      var num0 = num.toFixed(0);
+
+      if ((num2 - num0) !== 0) {
+        return num2.toString();
+      } else {
+        return num0.toString();
+      }
+
+    };
+
   });
