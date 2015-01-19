4f87d449d8b1e9a744627607473cd1f9aaca7ad4
samueljmurray <samueljmurray@gmail.com>
2015-01-19 14:00:56 +0000

    added support on front-end for reordering homepage components

--- app/components/core/directives.js
+++ app/components/core/directives.js
@@ -65,4 +65,86 @@ angular.module('wowApp')
         });
       }
     };
+  })
+  /**
+   * @ngdoc directive
+   * @name wowApp.directive:eventList
+   * @directive
+   *
+   * @description
+   * Renders event list component using it's relevant template
+   *
+   */
+  .directive('eventList', function($http, $compile) {
+    return {
+      restrict: 'A',
+      scope: true,
+      compile: function(element, attrs) {
+
+        return function(scope, element, attrs) {
+
+          var tpl = 'app/components/content_components/featuredEventsView.html';
+          $http.get(tpl)
+            .then(function(response) {
+              console.log(scope.component);
+              element.html($compile(response.data)(scope));
+            });
+
+        };
+      }
+    };
+  })
+  /**
+   * @ngdoc directive
+   * @name wowApp.directive:personList
+   * @directive
+   *
+   * @description
+   * Renders person list component using it's relevant template
+   *
+   */
+  .directive('personList', function($http, $compile) {
+    return {
+      restrict: 'A',
+      scope: true,
+      compile: function(element, attrs) {
+
+        return function(scope, element, attrs) {
+
+          var tpl = 'app/components/content_components/featuredPersonsView.html';
+          $http.get(tpl)
+            .then(function(response) {
+              element.html($compile(response.data)(scope));
+            });
+
+        };
+      }
+    };
+  })
+  /**
+   * @ngdoc directive
+   * @name wowApp.directive:pageList
+   * @directive
+   *
+   * @description
+   * Renders content page list component using it's relevant template
+   *
+   */
+  .directive('pageList', function($http, $compile) {
+    return {
+      restrict: 'A',
+      scope: true,
+      compile: function(element, attrs) {
+
+        return function(scope, element, attrs) {
+
+          var tpl = 'app/components/content_components/featuredPagesView.html';
+          $http.get(tpl)
+            .then(function(response) {
+              element.html($compile(response.data)(scope));
+            });
+
+        };
+      }
+    };
   });
