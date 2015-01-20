664b01167f7b4d57dcaa3ee57df2f3797bd81b73
samueljmurray <samueljmurray@gmail.com>
2015-01-20 10:32:56 +0000

    removed debugging helper

--- app/components/home/homeFactory.js
+++ app/components/home/homeFactory.js
@@ -33,8 +33,6 @@ angular.module('wowApp')
             // Loop through component perfomance list (featured events) JSON and correct date format for event start and end dates
             .success(function(components) {
 
-              console.log(components);
-
               angular.forEach(components.field_component, function(fieldComponent) {
 
                 if (fieldComponent.hasOwnProperty('field_list_performance')) {
