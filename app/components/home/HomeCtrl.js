c5b9145c6fd434aaa5705b092f16304775b3dc86
Dave Vernon <dave.vernon@southbankcentre.co.uk>
2015-02-13 17:54:24 +0000

    custom youtube play button and api code

--- app/components/home/HomeCtrl.js
+++ app/components/home/HomeCtrl.js
@@ -9,7 +9,7 @@
  * Defines the state and behaviour of the $scope for the homeView state
  */
 angular.module('wowApp')
-  .controller('HomeCtrl', function ($rootScope, $scope, homeFactory, eventFactory, personFactory, utilitiesFactory) {
+  .controller('HomeCtrl', function ($rootScope, $scope, $window, homeFactory, eventFactory, personFactory, utilitiesFactory) {
 
     /**
      * Method for getting the homepage landing page for this festival from the API
@@ -52,4 +52,77 @@ angular.module('wowApp')
     // See: http://lawsonry.com/p?11040
     // 
     // $scope.htmlReady();
-  });
+
+    var tag = document.createElement('script');
+    tag.src = "//www.youtube.com/player_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    /* var player;
+
+    // this function gets called when API is ready to use
+    //function onYouTubePlayerAPIReady() {
+    document.onYouTubeIframeAPIReady = function() {
+      // create the global player from the specific iframe (#video)
+      player = new YT.Player('video', {
+        events: {
+          // call this function when player is ready to use
+          'onReady': onPlayerReady
+        }
+      });
+    }
+
+    //function onPlayerReady(event) {
+
+      $scope.playVideo = function() {
+          console.log('hello');
+          player.playVideo();
+      }
+
+    // } */
+
+    var player;
+
+    $window.onYouTubeIframeAPIReady = function() {
+      player = new YT.Player('video', {
+        //height: '38',
+        //width: '640',
+        //videoId: '',
+        events: {
+          'onReady': onPlayerReady, 
+          'onStateChange': onPlayerStateChange
+        }
+      });
+
+      console.log('api ready');
+    }
+    
+    $window.onPlayerReady = function(event) {
+      player.playVideo();
+      
+      $scope.playVideo = function() {
+        // TODO use service
+        player.playVideo();
+      }
+
+      console.log('ready');
+    }
+    
+    $window.onPlayerStateChange = function(event) {
+      if (event.data == YT.PlayerState.ENDED) {
+        $timeout(function() {
+          $scope.setPlayerVolume($scope.playerVolume);
+          $scope.selectNextEntry();
+        });
+      }
+    }
+    
+    $scope.pauseVideo = function() {
+      //player.pauseVideo();
+    }
+    
+    $scope.playVideo = function() {
+      //player.playVideo();
+    }
+
+});
