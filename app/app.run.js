25aa33473f83ad72a8c21b3c0520761ff0687802
Dave Vernon <dave.vernon@southbankcentre.co.uk>
2015-02-26 17:34:49 +0000

    added new prefix to pass jshint

--- app/app.run.js
+++ app/app.run.js
@@ -15,7 +15,7 @@ angular
     scope.festivalId = scope.festivalAlias.substr(scope.festivalAlias.lastIndexOf('-') + 1);
 
     // Configure all $http requests to use a cache created by DSCacheFactory by default:
-    DSCacheFactory('defaultCache', {
+    new DSCacheFactory('defaultCache', {
         maxAge: 900000, // Items added to this cache expire after 15 minutes.
         cacheFlushInterval: 6000000, // This cache will clear itself every hour.
         deleteOnExpire: 'aggressive' // Items will be deleted from this cache right when they expire.
