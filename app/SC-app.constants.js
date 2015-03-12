5919d167dfa80e97f8e077b78ec786bc17fd4a4d
samueljmurray <samueljmurray@gmail.com>
2015-03-12 14:55:43 +0000

    moved app configuration variables into new appConfig constant

---
+++ app/SC-app.constants.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular
+  .module('SC-app')
+  .constant('angularMomentConfig', {
+
+    timezone: 'Europe/London'
+
+  })
+  .constant('appConfig', {
+
+  	// Module SC-app-festival
+  	// JSON path alias for festival content item
+    festivalAlias: 'women-world-festival-1',
+
+    // Module SC-app-festival
+    // Node ID of the festival content item, extract from the end of festivalAlias
+    get festivalId() {
+    	return this.festivalAlias.substr(this.festivalAlias.lastIndexOf('-') + 1);
+    },
+
+    // Module: SC-app-blog
+    // Used by disqus plugin
+    disqus_shortname: 'wow2015',
+
+    // Module: SC-app-event
+    // Vocabulary ID of the vocabulary that manages ticket types
+    ticketingVocabularyId: 4,
+
+    // Module: SC-app-utils
+    // Defines whether or not angular caching is enabled
+    angularCache: true
+
+  });
